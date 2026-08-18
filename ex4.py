41fe4125ab6c16473ee3fe4944ff2597d4426275
Fernando Jorge <decofernandinho@gmail.com>
2026-08-17 21:32:37 -0300

    adicionando ex4

---
+++ ex4.py
@@ -0,0 +1,8 @@
+n1 = int(input("Digite o primeiro numero: "))
+n2 = int(input("Digite o segundo numero: "))
+if n1 > n2:
+    print ("O primeiro numero e MAIOR")
+elif n2 > n1:
+    print ("O segundo numero e MAIOR")
+else:
+    print (" Ambos valores sao iguais")
