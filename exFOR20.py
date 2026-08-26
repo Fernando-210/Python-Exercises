45cc65bc73a8f5aa852ad922e022a828d68f3671
Fernando Jorge <decofernandinho@gmail.com>
2026-08-26 20:15:50 -0300

    adicionando ex20

---
+++ exFOR20.py
@@ -0,0 +1,12 @@
+from datetime import date
+atual = date.today().year
+totmaior= 1
+totmenor = 0
+for person in range(1,8):
+    nasc = int(input(f"Em que ano a {person} pessoa nasceu "))
+    idade = atual - nasc
+    if idade >= 21:
+        totmaior += 1
+    else:
+        totmenor += 1
+print (f"Tivemos ao todo {totmenor} menores de idade e {totmaior} maiores de idade")
