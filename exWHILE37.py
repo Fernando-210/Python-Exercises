86b15d08247991fb84ec9d7d359e7be89a3e376b
Fernando Jorge <decofernandinho@gmail.com>
2026-09-15 22:39:02 -0300

    adicionando ex37

---
+++ exWHILE37.py
@@ -0,0 +1,23 @@
+print ("="*15)
+print("  BANCO IRSH")
+print ("="*15)
+valor = int(input("Qual valor deseja sacar? R$"))
+total = valor
+ced = 50
+totced = 0
+while True:
+    if total >= ced:
+        total -= ced
+        totced += 1
+    else:
+        if totced > 0:
+            print(f"total de {totced} cedulas de R${ced}")
+        if ced == 50:
+            ced = 20
+        elif ced == 20:
+            ced = 10
+        elif ced == 10:
+            ced = 1
+        
+        if total == 0:
+            break
